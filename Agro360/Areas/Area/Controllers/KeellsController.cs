9ba3c2b2df49bc395690404861ba9cde782f2107
Newanga Wickramasinghe <newanga@live.com>
2021-02-18 16:13:04 +0530

    Add Admin KeellsController

---
+++ Agro360/Areas/Area/Controllers/KeellsController.cs
@@ -0,0 +1,108 @@
+﻿using Agro360.Data;
+using Agro360.Models;
+using Agro360.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agro360.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = Roles.AdminRole)]
+    public class KeellsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+        public KeellsController(ApplicationDbContext db,
+            UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { Data = await _userManager.GetUsersInRoleAsync(Roles.KeellsRole) });
+        }
+
+        [HttpGet]
+        //Create New Keels Staff
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        //Submit New Keels Staff Data
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(ApplicationUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    Email = model.mail,
+                    UserName = model.uname,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    NIC = model.NIC,
+                    Address = model.Address,
+                    ZipCode = model.ZipCode,
+                    PhoneNumber = model.ContactNo
+
+                };
+                var result = await _userManager.CreateAsync(user, model.Psswd);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, Roles.KeellsRole);
+                    return LocalRedirect("/Admin/Keells/Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> LockUnlock([FromBody] string id)
+        {
+            var User = _db.ApplicationUser.Where(u => u.Id == id).FirstOrDefault();
+            if (User == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+            if (User.LockoutEnd != null && User.LockoutEnd > DateTime.Now)
+            {
+                User.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                User.LockoutEnd = DateTime.Now.AddYears(100);
+            }
+            _db.ApplicationUser.Update(User);
+            await _db.SaveChangesAsync();
+
+            return Json(new { success = true, message = "Operation Successful." });
+        }
+
+    }
+}
