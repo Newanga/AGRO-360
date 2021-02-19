2828a649349fcab041b1ab6cb2291fb849453349
Newanga Wickramasinghe <newanga@live.com>
2021-02-19 22:56:09 +0530

    Add Roles.

---
+++ Agro360/Utility/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agro360.Utility
+{
+    //Use these constants to create the roles required in the application in the database.
+    public static class Roles
+    {
+        public const string AdminRole="Admin";
+        public const string FarmerRole ="Farmer";
+        public const string KeellsRole = "Keells";
+        public const string DOARole = "DOA";
+    }
+}
