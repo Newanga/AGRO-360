814603417036ac38054d8265801aad8943189a2c
Newanga Wickramasinghe <newanga@live.com>
2021-02-19 22:57:59 +0530

    Add Login class.

---
+++ Agro360/Models/LogInModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agro360.Models
+{
+    public class LogInModel
+    {
+        [DisplayName("Email or Username")]
+        [MinLength(3,ErrorMessage ="Invalid Email or Username")]
+        [Required(ErrorMessage ="Email Or Username is Required")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        [MinLength(7,ErrorMessage ="Invalid Password length")]
+        [Required]
+        public string Password { get; set; }
+
+        [DisplayName("Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
