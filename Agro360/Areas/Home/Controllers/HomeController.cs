3bbd34c3795c81758638e0db0391961b6f883f3d
Newanga Wickramasinghe <newanga@live.com>
2021-02-23 22:59:11 +0530

    Add Home Controller.

---
+++ Agro360/Areas/Home/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Agro360.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agro360.Areas.Home.Controllers
+{
+    [Area("Home")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
