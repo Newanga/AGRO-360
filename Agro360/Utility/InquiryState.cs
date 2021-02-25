db71082c9f82dbaa8c16af00d7f8191df04e4e8a
Newanga Wickramasinghe <newanga@live.com>
2021-02-25 09:54:36 +0530

    Add Inquiry and report states.

---
+++ Agro360/Utility/InquiryState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agro360.Utility
+{
+    public static class InquiryState
+    {
+        public const string Pending = "Pending";
+
+        public const string Solved = "Solved";
+    }
+}
---
+++ Agro360/Utility/ReportState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agro360.Utility
+{
+    //These constants represent various states a report submitted by a farmer could be before and after approval
+    public static class ReportState
+    {
+        //Farmer report states
+        public const string Farmer_Reviewable = "Farmer Reviewable";
+
+        //DOA report states
+        public const string DOA_Approved = "DOA Approved";
+        public const string DOA_Rejected = "DOA Rejected";
+        //Keels report states
+        public const string Keells_Approved = "Keells Approved";
+        public const string Keells_Rejected = "Keells Rejected";
+    }
+}
