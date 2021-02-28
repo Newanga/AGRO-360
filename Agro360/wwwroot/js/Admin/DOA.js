a98f2cbd4ff60d877ff420a503e07b71efd4bfff
Newanga Wickramasinghe <newanga@live.com>
2021-02-28 23:52:07 +0530

    Add addmin js

---
+++ Agro360/wwwroot/js/Admin/DOA.js
@@ -0,0 +1,69 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    loadList();
+})
+
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/Admin/DOA/GetAll/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "firstName", "width": "auto" },
+            { "data": "lastName", "width": "auto" },
+            { "data": "email", "width": "auto" },
+            { "data": "address", "width": "auto" },
+            { "data": "nic", "width": "auto" },
+            { "data": "phoneNumber", "width": "auto" },
+            {
+                "data": { id: "id", lockoutEnd: "lockoutEnd" },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        //currently user is locked
+                        return ` <div class="text-center">
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                                   <i class="fas fa-lock-open"></i> Unlock
+                                </a></div>`;
+
+                    }
+                    else {
+                        return ` <div class="text-center">
+                                <a class="btn btn-success text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                                    <i class="fas fa-lock"></i> Lock
+                                </a></div>`;
+                    }
+
+                }, "width": "auto"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
+
+function LockUnlock(id) {
+
+    $.ajax({
+        type: 'POST',
+        url: '/Admin/DOA/LockUnlock/',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                dataTable.ajax.reload();
+            }
+        }
+    });
+
+}
+
---
+++ Agro360/wwwroot/js/Admin/Harvest.js
@@ -0,0 +1,35 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+})
+
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/Admin/Harvest/GetAllHarvestTypes/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "harvestId", "width": "" },
+            { "data": "harvestType", "width": "auto" },
+            {
+                "data": "harvestId",
+                "render": function (data) {
+                    return ` <span style="display:block; text-align:center; margin:0 auto;">
+                                <a href="/Admin/Harvest/Edit?id=${data}" class=" btn-sm btn-warning text-black" style="cursor:pointer; width:100px;">
+                                    <i class="fas fa-edit"></i>
+                                </a>&nbsp
+
+                    </span>`;
+                }, "width": "auto"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
