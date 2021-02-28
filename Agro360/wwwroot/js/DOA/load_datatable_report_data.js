7dbeab276fa8c418b51cbbf68b20b284655c696a
Newanga Wickramasinghe <newanga@live.com>
2021-02-28 23:52:33 +0530

    Add DOA js

---
+++ Agro360/wwwroot/js/DOA/load_datatable_report_data.js
@@ -0,0 +1,95 @@
+﻿$(document).ready(function () {
+    loadList();
+})
+
+
+function loadList() {
+
+    var DT_load_pending = $('#DT_load_pending').DataTable({
+        destroy: true,
+        "ajax": {
+            "url": "/DOA/Report/GetAllPendingFarmerReports/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "date", "width": "auto" },
+            { "data": "time", "width": "auto" },
+            { "data": "harvest", "width": "auto" },
+            { "data": "quantity", "width": "auto" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <span class="text-center">
+                                 <a href="/DOA/Report/ViewPending?id=${data}" class="btn-sm btn-info text-white" style="cursor:pointer; width:100px;">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                    </span>`;
+                }, "width": "auto"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+
+    var DT_load_approved = $('#DT_load_approved').DataTable({
+        destroy: true,
+        "ajax": {
+            "url": "/DOA/Report/GetAllApprovedFarmerReports/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "date", "width": "auto" },
+            { "data": "time", "width": "auto" },
+            { "data": "harvest", "width": "auto" },
+            { "data": "quantity", "width": "auto" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <span class="text-center">
+                                 <a href="/DOA/Report/View?id=${data}" class="btn-sm btn-info text-white" style="cursor:pointer; width:100px;">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                    </span>`;
+                }, "width": "auto"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+
+    var DT_load_rejected = $('#DT_load_rejected').DataTable({
+        destroy: true,
+        "ajax": {
+            "url": "/DOA/Report/GetAllRejectedFarmerReports/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "date", "width": "auto" },
+            { "data": "time", "width": "auto" },
+            { "data": "harvest", "width": "auto" },
+            { "data": "quantity", "width": "auto" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <span class="text-center">
+                                 <a href="/DOA/Report/View?id=${data}" class="btn-sm btn-info text-white" style="cursor:pointer; width:100px;">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                    </span>`;
+                }, "width": "auto"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
